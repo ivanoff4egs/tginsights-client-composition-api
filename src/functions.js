74e655e8251276677cd22a8d9c1b3a28097e16c4
Serge I <ivanoff4eg.s@gmail.com>
2025-05-01 17:45:03 +0300

    format date

---
+++ src/functions.js
@@ -0,0 +1,5 @@
+export function formatDate(dateString) {
+  const date = new Date(dateString)
+  const options = {'hour12': false}
+  return `${date.toLocaleDateString()} ${date.toLocaleTimeString('uk-UA', options)}`
+}
