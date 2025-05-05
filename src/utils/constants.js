7cde006aa4196e7428e63b5bdcb3dc2cead8deb9
Serge I <ivanoff4eg.s@gmail.com>
2025-05-05 15:11:44 +0300

    channel

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const TimeRanges = {
+  HOUR: 'hour',
+  DAY: 'day',
+  WEEK: 'week',
+  MONTH: 'month'
+}
+
+export const ChannelActions = {
+  START: 'start',
+  STOP: 'stop',
+  LIKE: 'like',
+  DISLIKE: 'dislike'
+}
+
